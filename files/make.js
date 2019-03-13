8bf035a6288ec1c35b1e9a4d25bdeb82ac6ff4ad
Zach Bloomquist <github@chary.us>
2019-03-13 17:43:35 -0400

    1000

--- files/make.js
+++ files/make.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
-for (let i = 13; i <= 100; ++i) {
-    dir = exec(`cp /home/daniel/dev/sandbox/cypress-fetch-page/time/smurfs1.jpg /home/daniel/dev/sandbox/cypress-fetch-page/time/smurfs${i}.jpg`, function(err, stdout, stderr) {
+for (let i = 13; i <= 1000; ++i) {
+    dir = exec(`cp smurfs1.jpg smurfs${i}.jpg`, function(err, stdout, stderr) {
         if (err) {
             console.error(err);
         }
